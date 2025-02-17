a55df5a66a8e09764c3d042977e4d365564bec0b
son <43442285+sonphan18071999@users.noreply.github.com>
2025-02-17 11:24:35 +0700

    Update CLI @18

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import {routes} from './app.routes';
 import {provideClientHydration} from '@angular/platform-browser';
 import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import {provideStoreDevtools} from '@ngrx/store-devtools';
-import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import {customInterceptor} from './interceptors/custom.interceptor';
 import {errorHandlingInterceptor} from './interceptors/error-handling-interceptor.interceptor';
 import {provideStore} from "@ngrx/store";
